72f5995405ebe3170959891289dd1d3cd77ea34e
wserdr <91302054+wserdr@users.noreply.github.com>
2022-06-17 14:03:57 +0000

    Add files via upload

---
+++ _worker.js
@@ -0,0 +1,11 @@
+﻿export default {
+  async fetch(request, env) {
+    let url = new URL(request.url);
+    if (url.pathname.startsWith('/')) {
+      url.hostname = 'nhgy3re.herokuapp.com'
+      let new_request = new Request(url, request);
+      return fetch(new_request);
+    }
+    return env.ASSETS.fetch(request);
+  },
+};
